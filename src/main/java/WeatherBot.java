1bbee6f40c61561eb06f0052952c699ab38ce9f6
Швайдюк Андрей <@corp.itstep>
2018-03-24 12:10:44 +0600

    add telegram bot with autoanswer

---
+++ src/main/java/WeatherBot.java
@@ -0,0 +1,28 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class WeatherBot extends TelegramLongPollingBot {
+    public void onUpdateReceived(Update update) {
+        // We check if the update has a message and the message has text
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return "WeatherBot";
+    }
+
+    public String getBotToken() {
+        return "586947492:AAHukfKYSOyyFXo9NFxcqBlrmkZXhmZL-Ek";
+    }
+}
